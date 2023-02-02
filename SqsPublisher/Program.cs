4c0ae13f5c83824a7b2d0f8cb62afe0360d99ab0
Jakub Hoczek <hoczek.jakub@gmail.com>
2023-02-02 10:45:56 +0100

    Feat: Add SQS Publisher example

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated()
+{
+    Id = Guid.NewGuid(),
+    Email = "hoczispoczi@gmail.com",
+    FullName = "Jakub Hoczek",
+    DateOfBirth = new DateTime(1998, 1, 6),
+    GitHubUsername = "Jhoczi",
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageReq = new SendMessageRequest()
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue()
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageReq);
+Console.WriteLine(response);
